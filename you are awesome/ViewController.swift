fd72850cfc06dbf9222ad6ba30e9d20e9ccede06
Will Dailey <willdailey14@macbook-pro-3.bc.edu>
2017-08-28 23:30:13 -0400

    Initial Commit

---
+++ you are awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  you are awesome
+//
+//  Created by Will Dailey on 8/28/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
