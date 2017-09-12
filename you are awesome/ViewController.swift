a7fc194a412d2dcc4465c8f4e14af8cd2e844955
Will Dailey <willdailey14@macbook-pro-3.bc.edu>
2017-09-11 21:07:19 -0400

    through 2.19

--- you are awesome/ViewController.swift
+++ you are awesome/ViewController.swift
@@ -7,17 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var awesomeImage: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
-
+    @IBOutlet weak var soundSwitch: UISwitch!
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    let numberOfImages = 6
+    var soundNumber = -1
+    let numberOfSounds = 3
+    var lastSound = -1
+    
     //This code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        //can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            //check if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch
+            {
+                //if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound")
+            }
+            
+        } else {
+            //if reading in the NSDataAsset didn't work, tell the user / report the error.
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int{
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while lastNumber == newIndex
+        return newIndex
+    }
+    //FIXME: - Please repair this error
+    //TODO: - Update viewDidLoad()
+    //MARK: - Actions
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if !soundSwitch.isOn && soundNumber != -1 {
+            if lastSound != -1 {
+                awesomePlayer.stop()
+            }
+        }
+    }
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
         let messages = ["you are awesome",
@@ -25,38 +71,24 @@ class ViewController: UIViewController {
                         "you are fantastic",
                         "you are fun",
                         "you are nice"]
-        var newIndex = -1
         
-        repeat {
-        newIndex = Int(arc4random_uniform(UInt32(messages.count)))
-        } while index == newIndex
-        
-        index = newIndex
+        //show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         messageLabel.text = messages[index]
-        
-//        var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
-//        messageLabel.text = messages[randomIndex]
-        
-//        messageLabel.text = messages[index]
-//        index += 1
-//
-//        if index == messages.count {
-//            index = 0
-        }
-        
-//        let message1 = "You are da Bomb!"
-//        let message2 = "You are Great!"
-//        let message3 = "You are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        }
-//        else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        }
-//        else {
-//             messageLabel.text = message1
-//        }
     
+        //show an image
+        awesomeImage.isHidden = false
+        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeImage.image = UIImage(named: "image\(imageNumber)")
+        
+        if soundSwitch.isOn == true {
+            //Get a random number to use in our soundName file
+            soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+            
+            //play a sound
+            let soundName = "sound\(soundNumber)"
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
         }
-
+        
+    }
+}
